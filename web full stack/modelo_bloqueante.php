1f5f5f63c92082f9f8076b588afe9ad45307b9c8
montionH <66965471+montionH@users.noreply.github.com>
2021-10-28 11:48:40 -0600

    Add files via upload

--- web full stack/modelo_bloqueante.php
+++ web full stack/modelo_bloqueante.php
@@ -1,9 +1,9 @@
 <?php
 printf("abriendo archivo");
-$archivo_url=fopen(el_quijote.txt,"r");
+$archivo_url=fopen("el_quijote.txt","r");
 $texto="";
 while ($fragmento= fgets($archivo_url)){
-    $texto=$fragmento;
+    $texto.=$fragmento;
 }
 printf($texto);
 printf("<br> haciendo otra cosa");
